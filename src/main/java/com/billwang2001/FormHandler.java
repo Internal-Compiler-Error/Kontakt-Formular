607feffeb5027b6129ac86466802b3765f2ce81f
Bill Wang <bill79231725@gmail.com>
2018-11-19 12:34:42 -0500

    separate css files to better fit the cascading doctrine

--- src/main/java/com/billwang2001/FormHandler.java
+++ src/main/java/com/billwang2001/FormHandler.java
@@ -1,6 +1,5 @@
 package com.billwang2001;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,7 +9,6 @@ public class FormHandler {
     @RequestMapping("/form")
     public String form(@RequestBody ContactForm input) {
         ContactForm form = input;
-
         return "success";
     }
 }
