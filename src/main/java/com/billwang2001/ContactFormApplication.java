9237db87586ae5cbe12a3a054b9a8d18f06d03f2
Bill Wang <bill79231725@gmail.com>
2018-11-22 14:06:22 -0500

    code rearrange

--- src/main/java/com/billwang2001/ContactFormApplication.java
+++ src/main/java/com/billwang2001/ContactFormApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ContactFormApplication {
-    public static void main(String args[]){
-        SpringApplication.run(ContactFormApplication.class,args);
+    public static void main(String args[]) {
+        SpringApplication.run(ContactFormApplication.class, args);
     }
 }
