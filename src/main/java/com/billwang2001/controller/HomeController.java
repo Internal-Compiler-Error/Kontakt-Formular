b7e6412859eb07094ae08d5fd63f42f70112d6bb
Bill Wang <bill79231725@gmail.com>
2018-12-01 14:29:58 -0500

    fix redirect

--- src/main/java/com/billwang2001/controller/HomeController.java
+++ src/main/java/com/billwang2001/controller/HomeController.java
@@ -1,13 +1,13 @@
 package com.billwang2001.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HomeController {
 
     @RequestMapping("/")
-    public String contactForm() {
-        return "redirect:ContactForm.html";
+    public String index() {
+        return "index.html";
     }
 }
