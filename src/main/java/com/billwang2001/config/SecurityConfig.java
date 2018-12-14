cf7d175dc9ab70b0ef26d26522d3d2569baed2ef
Bill Wang <bill79231725@gmail.com>
2018-12-14 15:32:52 -0500

    disable csrf

--- src/main/java/com/billwang2001/config/SecurityConfig.java
+++ src/main/java/com/billwang2001/config/SecurityConfig.java
@@ -13,7 +13,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                    .antMatchers("/v1/**").permitAll();
+                .antMatchers("/v1/**").permitAll()
+                .and()
+                .csrf().disable();
     }
 
 }
