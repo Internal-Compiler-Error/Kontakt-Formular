229c2d80accf69fec3deafa2e2fb547ecff4175d
Bill Wang <bill79231725@gmail.com>
2018-12-18 11:52:01 -0500

    add magic fix for individual get request

--- src/main/java/com/billwang2001/model/ContactForm.java
+++ src/main/java/com/billwang2001/model/ContactForm.java
@@ -1,9 +1,12 @@
 package com.billwang2001.model;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import javax.persistence.*;
 
 @Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class ContactForm {
 
     @Id
